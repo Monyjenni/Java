3cbe2de9b665056a0efedcabfbb4c4a9bb7ece36
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-08-12 13:53:55 +0700

    init

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+	public static void main(String[] args) {
+    String Name= "Chansovanmony Yoeun";
+    int Age=19;
+    char BCll_Grade= 'C';
+    boolean Kind = false;
+    long phone_number= 085605126;
+    // in java we need to specify the data type else it wont work.
+
+    System.out.println(Name.equals("Chansovanmony Yoeun"));
+	}
+}
