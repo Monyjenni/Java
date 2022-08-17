533352cfc022c1d0bf18eced9b06d1b23942a0a3
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-08-17 12:41:53 +0700

    changes

---
+++ Variable1.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+      //String name= "mony";
+      //System.out.println(name.equals("Mony"));
+      int number1 = 4;
+      number1 ++;
+      number1 %= 5;
+      System.out.println(number1);
+       int a = 5;
+    int b = 7;
+    boolean result = a > b;
+    String name = "Mony";
+  // here they calculate all in sequential
+    int number = 5;
+    //number += 3;
+    //number -= 4;
+    //number *= 5;
+    number %= 7;
+    System.out.println(number);
+    System.out.println(result);
+    System.out.println(name.equals("bob"));
+      
+    }
+  }
+  
