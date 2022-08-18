4432273d5cdb6aabf60671afcd77db7266db3c63
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-08-18 17:54:39 +0700

    init

---
+++ Swapping.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void test (String[]args){
+        String x= "Water";
+        String y="Earth";
+         temp= x;
+         x = y;
+         y = temp;
+         System.out.println("X:"+x);
+         System.out.println("Y:" +y);
+
+
+    }
+}
